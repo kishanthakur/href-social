ce53721aad341760a1ba697b757babee8f4c1163
Kishanthakur <kishanthakur818@gmail.com>
2023-07-17 20:52:31 +0530

    resolved margin issue

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -37,32 +37,34 @@ export default function Profile() {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center mt-16">
-        <img
-          className="rounded-full w-36 h-36"
-          src="/Kishan-pp.png"
-          alt="Profile"
-        />
-        <p className="mt-5 mb-0 font-semibold text-xl text-center w-3/4 ">
-          {storedData.name}
-        </p>
-        <p className="mt-3 mb-4  line-clamp-5 italic font-mono text-lg text-center w-1/4 ">
-          {storedData.description}
-        </p>
-      </div>
-      {Object.entries(newLinks).map(([key, value], index) => {
-        return (
-          <a href={value} key={index} target="_blank" rel="noreferrer">
-            <div className="flex flex-wrap justify-center -mt-3">
-              <div
-                className={`${colorbg} ${colorText} m-4 w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 text-center font-bold p-4 rounded`}
-              >
-                <p>{key}</p>
+      <div>
+        <div className="flex flex-col justify-center items-center">
+          <img
+            className="rounded-full w-32 h-32 mt-16"
+            src="/Kishan-pp.png"
+            alt="Profile"
+          />
+          <p className="mt-5 mb-0 font-semibold text-xl text-center w-3/4 ">
+            {storedData.name}
+          </p>
+          <p className="mt-3 mb-4  line-clamp-5 italic font-mono text-lg text-center w-1/4 ">
+            {storedData.description}
+          </p>
+        </div>
+        {Object.entries(newLinks).map(([key, value], index) => {
+          return (
+            <a href={value} key={index} target="_blank" rel="noreferrer">
+              <div className="flex flex-wrap justify-center -mt-3">
+                <div
+                  className={`${colorbg} ${colorText} m-4 w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 text-center font-bold p-4 rounded`}
+                >
+                  <p>{key}</p>
+                </div>
               </div>
-            </div>
-          </a>
-        );
-      })}
+            </a>
+          );
+        })}
+      </div>
     </>
   );
 }
