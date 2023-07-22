dc2e43fd6e4e044c34695d8a67110d256aec623c
Kishanthakur <kishanthakur818@gmail.com>
2023-07-22 15:36:01 +0530

    added loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-yellow-500"></div>
+    </div>
+  );
+}
+
+export default Loading;
