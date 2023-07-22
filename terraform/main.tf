b9a068fa37690a11c10334c2f2a30add66eefd43
Harsiddh Dave <harsiddhdave44@outlook.com>
2023-07-22 23:22:49 +0530

    Added workflow and terraofrm automation

---
+++ terraform/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.0.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::070117509290:role/AWS_S3_Access_Terraform"
+    session_name = "terraform_deploy"
+  }
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "href-social"
+
+  force_destroy = true
+}
+
+
+resource "aws_s3_bucket_website_configuration" "website_bucket_web_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    sid    = "PublicReadGetObject"
+    effect = "Allow"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.website_bucket.arn}/*"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
