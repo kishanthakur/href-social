d4581d3d574294ec7d09cd6d7085d5d5d17b4046
Kishanthakur <kishanthakur818@gmail.com>
2023-07-27 23:22:08 +0530

    removed unused variables

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,11 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { App, Credentials } from "realm-web";
 import { useSelector, useDispatch } from "react-redux";
 import Loading from "./Loading";
-import {
-  STORE_DATA_IN_STATE,
-  STORE_EDIT_PROFILE_FLAG,
-  STORE_PREVIEW_FLAG,
-} from "../Reducers";
+import { STORE_EDIT_PROFILE_FLAG, STORE_PREVIEW_FLAG } from "../Reducers";
 
 export default function Profile() {
   const DATA_FROM_STATE = useSelector((state) => state.DATA.FORM_DATA);
@@ -60,7 +56,6 @@ export default function Profile() {
           setProfileData(dataByUserName);
           setLoading(false);
           dispatch(STORE_EDIT_PROFILE_FLAG(true));
-          const { _id, ...updatedDataByUsername } = dataByUserName;
         }
       }
       fetchData();
